4de7c926882db2ea3a00046189338ac532b1e142
kavunshiva <kavunshiva@gmail.com>
2017-06-29 10:37:13 -0400

    merge merge merge merge merge (merge)

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function findMinAndRemoveSorted(array){
+  return array.shift()
+}
 
+function merge(firstArray, secondArray){
+  let sortedArray = []
+  while (firstArray.length > 0 && secondArray.length > 0){
+    firstArray[0] < secondArray[0] ? sortedArray.push(findMinAndRemoveSorted(firstArray)) : sortedArray.push(findMinAndRemoveSorted(secondArray))
+  }
+  return sortedArray.concat(firstArray).concat(secondArray)
+}
+
+function mergeSort(array){
+  let firstHalf = array.slice(0, array.length/2)
+  let secondHalf = array.slice(array.length/2, array.length)
+
+  if (array.length < 2){
+    return array
+  } else {
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+}
